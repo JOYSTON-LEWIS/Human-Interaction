66720563dfffef81370744137258e130839ffb15
JOYSTON-LEWIS <joyston2010@gmail.com>
2024-11-10 21:57:08 +0530

    static asset files used instead of links of videos and images

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent 
+// implements AfterViewInit 
+{
+  // isScrolled = false;
 
+  // @ViewChild('secondSection', { static: false }) secondVideoContainer!: ElementRef;
+
+  // ngAfterViewInit() {
+  //   this.checkScrollPosition(); // Initial check when the view is initialized
+  // }
+
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   console.log("called")
+  //   this.checkScrollPosition();
+  // }
+
+  // checkScrollPosition() {
+  //   console.log("called second")
+  //   if (this.secondVideoContainer?.nativeElement) {
+  //     const rect = this.secondVideoContainer.nativeElement.getBoundingClientRect();
+  //     this.isScrolled = rect.top <= window.innerHeight / 2;
+  //   }
+  // }
 }
